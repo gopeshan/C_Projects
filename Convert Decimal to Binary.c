d726b2d8e355e320106ccb1c0b38e3cfa33625b5
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-07 00:43:45 -0500

    Update Convert Decimal to Binary.c
    Added function for binary printing logic. Added prompt to indicate binary number.

--- Convert Decimal to Binary.c
+++ Convert Decimal to Binary.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void printBinary(int number) {
+    for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
+        int bit = (number >> i) & 1;
+        printf("%d", bit);
+    }
+}
 
 int main() {
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
-        int bit = (number >> i) & 1;
-        printf("%d", bit);
-    }
+    printf("Binary representation: ");
+    printBinary(number);
 
     printf("\n");
 
     return 0;
 }
-
