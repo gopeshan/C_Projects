2edc6ec79cfe30837ff67d0ca17a5968d311c9a7
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-07 00:51:15 -0500

    Update Reverse Text.c
    Made reversing string into function. Made adjustments for formatting.

--- Reverse Text.c
+++ Reverse Text.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+void reverseString(char *str) {
+    int length = strlen(str);
+
+    for (int i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+}
+
 int main() {
     char text[100];
 
     printf("Enter text: ");
     fgets(text, sizeof(text), stdin);
 
-    int length = strlen(text);
+    // Remove the newline character from the input
+    text[strcspn(text, "\n")] = '\0';
 
-    for (int i = length - 1; i >= 0; i--) {
-        printf("%c", text[i]);
-    }
+    printf("Reversed Text: ");
+    reverseString(text);
 
     return 0;
 }
-
