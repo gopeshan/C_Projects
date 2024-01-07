c83ee9071cca2b2f8d42cf51ad89bce89bc50c41
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-07 00:42:00 -0500

    Update Convert Decimal to Binary.c
    Used loop and runs through to convert.

--- Convert Decimal to Binary.c
+++ Convert Decimal to Binary.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
+        int bit = (number >> i) & 1;
+        printf("%d", bit);
+    }
+
+    printf("\n");
+
+    return 0;
+}
 
