7b658097b10de2f2ab622a52084094a6d1cf12a5
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-07 00:49:36 -0500

    Update Reverse Text.c
    Ask the user for input. Calculate the length of user input. Reverse by iterating through characters of the string in reverse order.

--- Reverse Text.c
+++ Reverse Text.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char text[100];
+
+    printf("Enter text: ");
+    fgets(text, sizeof(text), stdin);
+
+    int length = strlen(text);
+
+    for (int i = length - 1; i >= 0; i--) {
+        printf("%c", text[i]);
+    }
+
+    return 0;
+}
 
